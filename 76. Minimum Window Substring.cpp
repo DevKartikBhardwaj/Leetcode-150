bf178ad2f1c4924ff30cc30dc67da2768ef5b8b8
Kartik Bhardwaj <102840713+DevKartikBhardwaj@users.noreply.github.com>
2025-09-09 21:33:12 +0530

    optimal solution added

--- 76. Minimum Window Substring.cpp
+++ 76. Minimum Window Substring.cpp
@@ -34,3 +34,42 @@ public:
         return s.substr(left,right-left+1);
     }
 };
+
+
+//optimal solution
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int a=0,b=0,freq=0,minLen=INT_MAX,left=-1,right=-1;
+        vector<int>hash(256,0);
+        for(char ch:t){
+            hash[ch]+=1;
+        }
+        while(b<s.size()){
+            char currentChar=s[b];
+            if(hash[currentChar]>0){
+                freq+=1;
+            }
+            hash[currentChar]-=1;
+
+            while(freq==t.size()){
+                if(minLen>b-a+1){
+                    minLen=b-a+1;
+                    left=a;right=b;
+                }
+                char prevChar=s[a];
+                hash[prevChar]+=1;
+                if(hash[prevChar]>0){
+                    freq-=1;
+                }
+                a++;
+            }
+            b++;
+        }
+        if(left==-1){
+            return "";
+        }
+        
+        return s.substr(left,right-left+1);
+    }
+};
